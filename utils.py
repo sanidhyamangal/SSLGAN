3683a443e886d5fff151069c5772c8c03a1c0db5
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2022-04-17 18:50:31 -0500

    fixed: import error in utils

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ import torch  # for pytorch based stuff
 import torch.nn as nn  # for nn stuff
 import torchvision.utils as vutils  # for plotting and other part
 from torchvision import transforms  # for transforming the vision related ops
+from typing import List
 
 
 def create_dirs_if_not_exists(path) -> None:
